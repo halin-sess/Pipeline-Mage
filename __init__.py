643af31013d524e579a7a1e7e1c007ac3acd1db2
Halin <53790728+halin-sess@users.noreply.github.com>
2026-07-07 18:57:18 -0400

    Update __init__.py
    ✨ Add Two Actions to Prepare Mesh Section
    - Add New Flat Material
    Creates and hooks up new mat to active mesh. Image Texture size is selected during setup. Saves time going to the shader editor.
    - Snap UVs to Selection
    A command that moves all uvs of a mesh to a quadrant. Useful for quickly organizing uvs.  Upper Left, Upper Right, Lower Left, or Lower Right.

--- __init__.py
+++ __init__.py
@@ -15,6 +15,10 @@ import os
 
 import bpy
 
+# Provide Python Wrappers
+
+import gpu
+
 # For Custom Icons
 
 import bpy.utils.previews
@@ -136,9 +140,164 @@ class PIPELINEMAGE_send_selected_to_collection(bpy.types.Operator):
        
         self.report({'INFO'}, "Moved to new collection! :D")
         return {'FINISHED'}
+    
+    
+class PIPELINEMAGE_add_flat_material_and_apply_to_selected_mesh(bpy.types.Operator):
+    bl_idname = "pipeline_mage.new_flat_material"
+    bl_label = "Create Flat Material With Empty Image Node and Attach to Selected"
+    bl_description = "Create flat material with empty image node and attach to selected mesh"
+    
+    # User Selects Size (in px) of Texture. 16, 32, 64, 128, 256, 512, 1024, or 2048
+    texture_size: bpy.props.EnumProperty(
+        name= "Texture Size",
+        description="Choose the size of the new texture",
+        items = [
+           ('16', "16 x 16", "Create a 16px texture"),
+           ('32', "32 x 32", "Create a 32px texture"),
+           ('64', "64 x 64", "Create a 64px texture"),
+           ('128', "128 x 128", "Create a 128px texture"),
+           ('256', "256 x 256", "Create a 256px texture"),
+           ('512', "512 x 512", "Create a 512px texture"),
+           ('1024', "1024 x 1024", "Create a 1024px texture"),
+           ('2048', "2048 x 2048", "Create a 2048px texture"), 
+        ],
+        default='1024'
+        
+    )
+    
+    def invoke(self, context, event):
+       return context.window_manager.invoke_props_dialog(self)
+    
+    
+    def execute(self, context):
+        obj = context.object
+        
+        if obj is None:
+            self.report({'WARNING'}, "No active object selected")
+            return {'CANCELLED'}
+        
+        if obj.type != 'MESH':
+            self.report({'WARNING'}, "Active object is not a mesh")
+            return {'CANCELLED'}
+        
+        mesh = obj.data
+        size = int(self.texture_size)
+        
+        image = bpy.data.images.new(
+           name=f"T_{mesh.name}_{size}",
+           width=size,
+           height=size,
+           alpha=True
+        
+        )
+        
+        # If Material already exists on Object, don't create a dupe
+        if "M_" + mesh.name in mesh.materials:
+            self.report({'INFO'}, "Material already exists")
+            return {'FINISHED'}
+        
+        # Add a New Material named M_ + Selected Active Object's Name
+        bpy.data.materials.new(name="M_" + mesh.name)
+        
+        mat = bpy.data.materials.get("M_" + mesh.name)
+        
+        # Assign Material to Object, stick it in slot [0]
+        
+        if obj.data.materials:
+           obj.data.materials[0] = mat
+        else:
+           # no slots
+           obj.data.materials.append(mat)
+            
+            
+        mat.use_nodes = True
+        nodes = mat.node_tree.nodes
+        links = mat.node_tree.links
+        
+        
+        image_node = nodes.new('ShaderNodeTexImage')
+        image_node.location = (-300, 0)
+        image_node.image = image
+        
+        
+        # Why is an Image Texture Node called by its type name, but not a BSDF!? YARGH I love you Blender <3
+        # There's probs a real reason. I'm sure
+        
+        bsdf_shader_node = nodes.get('Principled BSDF')
+        
+        if bsdf_shader_node is None:
+            bsdf_shader_node = nodes.new(type='ShaderNodeBsdfPrincipled')
+            bsdf_shader_node.location = (300, 0)
+            
+        
+        links.new(image_node.outputs['Color'], bsdf_shader_node.inputs['Base Color'])
+        
+        self.report({'INFO'}, "Created a New Material For Object!")
+        return {'FINISHED'}
+    
+class PIPELINEMAGE_snap_uvs_to_selected_quadrant(bpy.types.Operator):
+    bl_idname = "pipeline_mage.snap_uvs_to_selected_quadrant"
+    bl_label = "Snap UVs to Selected Quadrant"
+    bl_description = "Snap UVs to selected quadrant"
+     
+     
+    region: bpy.props.EnumProperty(
+       name="UV Region",
+       items=[
+          ('0', "Upper Left", "Move UVS to upper-left region"),
+          ('1', "Upper Right", "Move UVS to upper-right region"),
+          ('2', "Lower Left", "Move UVS to lower-left region"),
+          ('3', "Lower Right", "Move UVs to lower-right region"),
+       ],
+       default='1'
+    )
+    
+    
+    def invoke(self, context, event):
+        return context.window_manager.invoke_props_dialog(self)
+    
+    def execute(self, context):
+        obj = context.object
+        
+        if obj is None:
+            self.report({'WARNING'}, "No active object selected")
+            return {'CANCELLED'}
+        
+        if obj.type != 'MESH':
+            self.report({'WARNING'}, "Active object is not a mesh")
+            return {'CANCELLED'}
+        
+        mesh = obj.data
+        uv_layer = mesh.uv_layers.active
+    
+        if uv_layer is None:
+          self.report({'Warning'}, "Object has no UV Map")
+          return {'CANCELLED'}  
+    
+        regions = {
+            '0': (0.0, 0.5),
+            '1': (0.5, 0.5),
+            '2': (0.0, 0.0),
+            '3': (0.5, 0.0),
+    
+        }
+    
+        offset_u, offset_v =regions[self.region]
+     
+        for loop in mesh.loops:
+            uv = uv_layer.data[loop.index].uv
+        
+            uv.x = uv.x * 0.5 + offset_u
+            uv.y = uv.y * 0.5 + offset_v
+        
+        mesh.update()
+     
+        self.report({'INFO'}, "Moved UVs to Selected Region")
+        return {'FINISHED'}
+
             
 
-# Prepare Mesh
+# Prepare Mesh Section
 
 def draw_prepare_section(layout, context, props):
     box, is_open = draw_section(layout, props, "show_prepare", "Prepare Mesh")
@@ -163,10 +322,15 @@ def draw_prepare_section(layout, context, props):
     col.operator("pipeline_mage.batch_rename", text= "Batch Rename", icon = "GREASEPENCIL")
     # Send Selected to New Collection
     col.operator("pipeline_mage.new_collection", text= "Send Selected to Collection", icon = "COLLECTION_NEW")
-    # Add Material
-    col.operator("material.new", text= "Add Material (WIP)", icon = "MATERIAL")
-
-        
+    # Add Flat Material With Empty Texture Node
+    col.operator("pipeline_mage.new_flat_material", text= "Add New Flat Material", icon = "MATERIAL")
+    col.separator()
+    # Snap Selected UV Island to Specific Region on Pixel Grid
+    col.operator("pipeline_mage.snap_uvs_to_selected_quadrant", text= "Snap UVs to Selection", icon = "SNAP_GRID")
+    # Create a New Gradient and Add it to Gradient Library
+    # col.operator("", text= "Add New Gradient to Library", icon = "FILEBROWSER")
+    # Snap a Gradient to Specified Region on Pixel Grid
+    # col.operator("", text= "Snap Gradient to Region", icon = "NODE_TEXTURE")
         
         
 # Validate Mesh
@@ -234,6 +398,8 @@ classes = (
     PIPELINEMAGE_add_col_attribute,
     PIPELINEMAGE_batch_rename,
     PIPELINEMAGE_send_selected_to_collection,
+    PIPELINEMAGE_add_flat_material_and_apply_to_selected_mesh,
+    PIPELINEMAGE_snap_uvs_to_selected_quadrant,
     VIEW3D_PT_pipeline_mage,
 )
 
